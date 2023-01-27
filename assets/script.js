2040fb9146c7b5b263a7186883d9c477b61b0488
MariuszWiacek <wiacekm.uber@gmail.com>
2023-01-27 18:35:52 +0000

    search button

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,26 @@
-var APIKey = "583d686b6a9be77ec15bfd1ee5c6eaa2";
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey;
 
 
 
 
 $("#search-button").on("click", function(event) {
-
-
+  var userInput = $('#search-input').val();
+  var APIKey = "583d686b6a9be77ec15bfd1ee5c6eaa2";
+  var queryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + APIKey;
     // Preventing the submit button from trying to submit the form
     // We're optionally using a form so the user may hit Enter to search instead of clicking the button
     event.preventDefault();
-  
-    // Here we grab the text from the input box
-    var movie = $("#movie-input").val();
-  
-    // Here we construct our URL
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
     
-  
+    console.log("bla bla bla " + queryUrl);
     // Write code between the dashes below to hit the queryURL with $ajax and take the response data
    
     $.ajax({
-      url: queryURL,
+      url: queryUrl,
       method: "GET"
     }).then(function(response) {
       
       // Create and save a reference to new empty table div
      
-      $("#movie-view").text(JSON.stringify(response));
+      $("#weather-view").text(JSON.stringify(response));
     });})
 
 
