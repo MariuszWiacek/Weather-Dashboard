40ffd20b9e59be95d9eb0aaaade42cd55e3c24c0
MariuszWiacek <wiacekm.uber@gmail.com>
2023-01-31 18:31:17 +0000

    another AJAX call for forecast to get 5day weather, trying to fix bugs

--- assets/script.js
+++ assets/script.js
@@ -27,8 +27,7 @@ var userInput = $('#search-input').val().toUpperCase()
 // My personal API key 
 
 var APIKey = "583d686b6a9be77ec15bfd1ee5c6eaa2";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey;
-    
+
     
 event.preventDefault(); // Preventing the submit button from trying to submit the form
 $("#today").html("")    // clears the form before submitting another one
@@ -38,9 +37,30 @@ $("#forecastheader").html("")
     
 // AJAX call 
    
+//gets data from first uery url
+var queryUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + APIKey;
+
+
 $.ajax({
-url: queryURL,
-method: "GET"
+  url: queryUrl,
+  method: "GET"
+}).then(function (FirstResponse) {
+ 
+ var latitude = (FirstResponse[0].lat).toFixed(2)
+ var longitude = (FirstResponse[0].lon).toFixed(2)
+
+//forecast query
+ var forQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey 
+
+
+
+
+
+
+//  another AJAX call for forecast
+$.ajax({
+  url: forQueryURL,
+  method: "GET",
 }).then(function(response) {
   todayWeather.empty()
 
@@ -54,9 +74,9 @@ method: "GET"
 
 
 // variables for weather results 
-var celsiusTemperature = Math.floor(response.main.temp - 273.15);
-var humidity = response.main.humidity;
-var wind = response.wind.speed;
+var celsiusTemperature  = ((response.list[0].main.temp) - 273.15).toFixed(0)
+var humidity = (response.list[0].main.humidity)
+var wind = (response.list[0].wind.speed).toFixed(1)
 
   // prepend current date using moments.js
 var CurrentDate = moment().format('Do MMMM YYYY ');
@@ -68,7 +88,7 @@ $("#today").prepend("Today is " + " " + " " + CurrentDate + "<br>" + "Current we
 
 // get weather icon image
 
-var iconCode = response.weather[0].icon
+var iconCode = response.list[0].weather[0].icon;
 
 var imageSource = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png'
 var image = $('<img>')
@@ -85,56 +105,62 @@ $("#today").append("Wind speed: " + wind + " m/s" + "<br>");
 
 
 // 5 DAY WEATHER FORECAST SECTION
+function forecast() {
+
+// variables for forecast using loop
+for (var i = 5; i < response.list.length; i +=8) {
+
+  var image = $("<img>").append(iconValue);
+  var iconValue = response.list[i].weather[0].icon
+ 
+  // var forecastTemp = 
+  var forTemp = ((response.list[i].main.temp) - 273.15).toFixed(2);
+  
+  // var forecastHum = 
+   
+  var forHum = response.list[i].main.humidity.toFixed(0);
+  
+  
+  // var forecastWind 
+  var forWind = response.list[i].wind.speed.toFixed(0);
+  
+  
+  
+  }
+  
 
 
 var forecastHeader = $("<p>").text("5 Day Weather Forecast" + " " + "for" + " " + userInput)
 $("#forecastheader").append(forecastHeader);
 
-// variables for forecast
-
-
-
-// var forecastTemp = 
-
-
-// var forecastHum = 
-
-// var forecastWind = 
-
-
 // creating 5 day forecast cards using loop 
-for (var i = 0; i < 5; i++) {
+for (var j = 0; j < 5; j++) {
   
-  var forecastDate = moment().add(i + 1, 'days').format("DD/MM/YYYY");
-  var forecastCard = $("<div>")
-
-// forecast contents
-var iconValue = []
-  var forecastCardContentIcon = $("<p>").append(iconValue);
-  forecastCard.append(forecastCardContentIcon);
-
-  var forecastCardContentTemp = $("<p>").text("Temperature:" + " "  + celsiusTemperature + "°C" );
-  forecastCard.append(forecastCardContentTemp);
-
-  var forecastCardContentHum = $("<p>").text("Relative humidity: " + humidity + "%");
-  forecastCard.append(forecastCardContentHum);
-
-  var forecastCardContentWind = $("<p>").text("Wind speed: " + wind + " m/s");
-  forecastCard.append(forecastCardContentWind);
-  
+  var forecastDate = moment().add(j + 1, 'days').format("DD/MM/YYYY");
+  var forecastCard = $("<div>") 
   forecastCard.addClass('bg-info m-1 p-1 rounded text-center text-dark border border-dark col-lg-2 col-sm-4 col-md-4 col-10 align-items-center text-wrap')
   forecastCard.prepend("<h4>" + forecastDate + " </h4>");
-  $("#forecast").append(forecastCard);
-}
   
 
+  
+$("#forecast").append(forecastCard);
+forecastCard.append(image);
+forecastCard.append("<br>" + "Temp :" + " "  + forTemp[j] + "°C" + "<br>")
+forecastCard.append("Humidity: " + forHum[j] + "%" + "<br>");
+forecastCard.append("Wind speed: " + forWind[j] + " m/s" + "<br>");
+
+}  
+
+}
+
+forecast()
   
   // Add results to local storage
 localStorage.setItem('history', JSON.stringify(storage));
 storage.push(userInput) ;
 
   
-})})
+})})})
 
 
 
